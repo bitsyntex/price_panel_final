dbb5d3b64f01703976af863e5eacd306cce51b85
bitsyntex <bitsyntex@gmail.com>
2020-09-06 10:36:41 +0200

    commit

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ setInterval(function () {
     prices.getPrice();
 }, 500);
 
-//
-// server.listen(process.env.PORT || 3000, () => {
-//     console.log("Server started on port 3000.");
-// });
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Server started on port 3000.");
+});
 
 module.exports = app;
