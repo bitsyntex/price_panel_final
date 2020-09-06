2c6ee3f8dcb8f135ee564995befb3fa99b5e0c68
bitsyntex <bitsyntex@gmail.com>
2020-09-06 08:55:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ setInterval(function () {
 }, 500);
 
 
-server.listen(process.env.PORT || 3000, () => {
-    console.log("Server started on port 3000.");
-});
+// server.listen(process.env.PORT || 3000, () => {
+//     console.log("Server started on port 3000.");
+// });
 
 module.exports = app;
